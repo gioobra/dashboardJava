afc1961037da24f4404db2eeffd0ef05638824ff
gioobra <gioobrandino@gmail.com>
2025-05-24 17:15:40 -0300

    adicionado o gráfico de uso da CPU

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -17,20 +17,32 @@ import javafx.scene.chart.XYChart;
 import model.Processes;
 
 public class MainController {
-    @FXML private ProgressBar barraUsoCPU;
-    @FXML private Label valorCPU;
-    @FXML private ProgressBar barraUsoMemoria;
-    @FXML private Label valorMemoria;
-    @FXML private Label valorNProcessos;
-    @FXML private Label valorThreads;
-    @FXML private ListView<String> CPUinfo;
-    @FXML private TableView<Object> tabelaProcessos;
-    @FXML private Button detalhes;
-    @FXML private LineChart<Number,Number> graficoCPU;
-    @FXML private PieChart graficoRAM;
+    @FXML
+    private ProgressBar barraUsoCPU;
+    @FXML
+    private Label valorCPU;
+    @FXML
+    private ProgressBar barraUsoMemoria;
+    @FXML
+    private Label valorMemoria;
+    @FXML
+    private Label valorNProcessos;
+    @FXML
+    private Label valorThreads;
+    @FXML
+    private ListView<String> CPUinfo;
+    @FXML
+    private TableView<Object> tabelaProcessos;
+    @FXML
+    private Button detalhes;
+    @FXML
+    private LineChart<Number, Number> graficoCPU;
+    @FXML
+    private PieChart graficoRAM;
     /*
-    @FXML private TableColumn<Integer, Integer> colunaPid;
-    @FXML private TableColumn<String, String> colunaNome;
+    @FXML private TableColumn<Integer, Integer> colunaNome;
+    @FXML private TableColumn<String, String> colunaPID;
+    @FXML private TableColumn<String, String> colunaUser;
     @FXML private TableColumn<Double, Double> colunaCpuPercent;
     @FXML private TableColumn<Double, Double> colunaMemoriaPercent;
      */
@@ -38,16 +50,17 @@ public class MainController {
     private CpuMonitor cpuMonitor;
     //private CPU cpu;
     private XYChart.Series<Number, Number> valoresCPU;
-    //private int tempo = 0;
-    //private static final int MAX_PONTOS_GRAFICO_CPU= 60;
+    private XYChart.Series<Number, Number> valoresCPUIdle;
+    private int tempo = 0;
+    private static final int MAX_PONTOS_GRAFICO_CPU = 60;
     //private Processes process;
     private ScheduledService<SystemDataSnapshot> updateService;
 
-    private static class SystemDataSnapshot{
+    private static class SystemDataSnapshot {
         double cpuUsage;
-        double NumProcesses;
-        double NumThreads;
-
+        double cpuIdle;
+        long numProcesses;
+        long numThreads;
     }
 
     @FXML
@@ -73,10 +86,16 @@ public class MainController {
         valoresCPU = new XYChart.Series<>();
         valoresCPU.setName("Uso CPU (%)");
         graficoCPU.getData().add(valoresCPU);
+
+        valoresCPUIdle = new XYChart.Series<>();
+        valoresCPUIdle.setName("CPU Idle (%)");
+        graficoCPU.getData().add(valoresCPUIdle);
+
         graficoCPU.setAnimated(false);
-        graficoCPU.getXAxis().setLabel("Tempo (intervalos de 5s)");
-        graficoCPU.getYAxis().setLabel("Uso (%)");
+        graficoCPU.getXAxis().setLabel("Tempo (s)");
+        graficoCPU.getYAxis().setLabel("Uso/Idle (%)");
     }
+
     /*private void setupProcessTableColumns() {
         colunaPid.setCellValueFactory(new PropertyValueFactory<>("pid"));
         colunaNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
@@ -95,9 +114,10 @@ public class MainController {
                         SystemDataSnapshot snapshot = new SystemDataSnapshot();
 
                         snapshot.cpuUsage = cpuMonitor.getCpuUsage(0);
+                        snapshot.cpuIdle = cpuMonitor.getCpuIdle(0);
 
-                        snapshot.NumProcesses = cpuMonitor.getTotalProcesses();
-                        snapshot.NumThreads = cpuMonitor.getTotalThreads();
+                        snapshot.numProcesses = cpuMonitor.getTotalProcesses();
+                        snapshot.numThreads = cpuMonitor.getTotalThreads();
 
                         return snapshot;
                     }
@@ -112,24 +132,26 @@ public class MainController {
         updateService.setOnSucceeded(event -> {
             SystemDataSnapshot snapshot = updateService.getValue(); // Pega os dados coletados
 
+            long tempoNoGrafico = (long)this.tempo * 5;
 
             // Atualizar barra e label de CPU
             barraUsoCPU.setProgress(snapshot.cpuUsage / 100.0);
             valorCPU.setText(String.format("CPU: %.2f %%", snapshot.cpuUsage));
 
             // Atualizar gráfico de CPU
-            //adicionarPontoGraficoCPU(snapshot.cpuUsage);
+            adicionarPontoGraficoCPU(tempoNoGrafico, snapshot.cpuUsage, snapshot.cpuIdle);
 
             // Atualizar labels de processos e threads
-            valorNProcessos.setText(String.format("Número de Processos: %d", snapshot.NumProcesses));
-            valorThreads.setText(String.format("Threads: %d", snapshot.NumThreads));
+            valorNProcessos.setText(String.format("Número de Processos: %d", snapshot.numProcesses));
+            valorThreads.setText(String.format("Threads: %d", snapshot.numThreads));
 
 
             valorMemoria.setText("Memoria: - % (Não implementado)");
 
 
-
             System.out.println("Atualização da UI: População da tabela de processos ainda pendente no CpuMonitor.");
+
+            this.tempo++;
 
         });
 
@@ -142,49 +164,38 @@ public class MainController {
 
         updateService.start(); // Inicia o serviço
     }
+
     public void shutdown() {
         if (updateService != null) {
             updateService.cancel();
             System.out.println("Serviço de atualização parado.");
         }
     }
-        /*barraUsoCPU.setProgress(usoCpuInicial / 100.0);
-        valorCPU.setText(String.format("CPU: %.2f %%", usoCpuInicial));
 
-        barraUsoMemoria.setProgress(); // mudar quando tiver
-        valorMemoria.setText("Memoria: - %");
 
-        valorNProcessos.setText("Número de Processos: " + this.process.getTotalProcesses());
-        valorThreads.setText("Threads: " + this.process.getTotalThreads());
+    private void adicionarPontoGraficoCPU(long xValue, double usoCPU, double idleCPU) {
+        if (valoresCPU != null) {
+            valoresCPU.getData().add(new XYChart.Data<>(xValue, usoCPU));
 
-        valoresCPU = new XYChart.Series<>();
-        valoresCPU.setName("Uso CPU (%)");
-        graficoCPU.getData().add(valoresCPU);
-        graficoCPU.setAnimated(false);
-        graficoCPU.getXAxis().setLabel("Tempo (s)");
-        graficoCPU.getYAxis().setLabel("Uso (%)");
+            if (valoresCPU.getData().size() > MAX_PONTOS_GRAFICO_CPU) {
+                valoresCPU.getData().remove(0);
+            }
+        }
+        else{
+            System.err.println("A série 'valoresCPU' para o gráfico de CPU não foi inicializada!");
+        }
 
-        CPUinfo.getItems().add("CPU: " + this.cpu.getCpuName());
-        CPUinfo.getItems().add("Cores: " + this.cpu.getNumberOfCores());
-        CPUinfo.getItems().add("Uso CPU: " + this.cpu.getCpuInUse() + "%");
-        CPUinfo.getItems().add(("CPU em idle: ") + this.cpu.getCpuInIdle() + "%");
-
-        /*
-        colunaPid.setCellValueFactory(new PropertyValueFactory<>("pid"));
-        colunaNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
-        colunaCpuPercent.setCellValueFactory(new PropertyValueFactory<>("cpuPercent"));
-        colunaMemoriaPercent.setCellValueFactory(new PropertyValueFactory<>("memoriaPercent"));
-         */
-
-    }
-    /*private void adicionarPontoGraficoCPU(double usoCPU){
-        graficoCPU.getData().add(new XYChart.Data<>(tempo++, usoCPU));
-
-        if (graficoCPU.getData().size() > MAX_PONTOS_GRAFICO_CPU) {
-            graficoCPU.getData().remove(0);
+        if (valoresCPUIdle != null) {
+            valoresCPUIdle.getData().add(new XYChart.Data<>(xValue, idleCPU));
+            if (valoresCPUIdle.getData().size() > MAX_PONTOS_GRAFICO_CPU) {
+                valoresCPUIdle.getData().remove(0);
+            }
+        }
+        else{
+            System.err.println("A série 'valoresCPUIdle' não foi inicializada!");
         }
     }
-
+}
     /*@FXML
     private void detalhes() {
 
